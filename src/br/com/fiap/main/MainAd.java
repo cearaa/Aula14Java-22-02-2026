5d7960261598881afa3c001e718dd989716f39b5
Rikk <tarikmoussa90@gmial.com>
2026-04-22 16:47:59 -0300

    finalizaçao do atv

--- src/br/com/fiap/main/MainAd.java
+++ src/br/com/fiap/main/MainAd.java
@@ -17,22 +17,26 @@ public class MainAd {
                 switch (opcao){
                     case 1:
                         auxiliar = JOptionPane.showInputDialog("Dígite um número inteiro: ");
-                        auxiliar = JOptionPane.showInputDialog("Dígite outro número inteiro: ");
                         numInt1 = Integer.parseInt(auxiliar);
+                        auxiliar = JOptionPane.showInputDialog("Dígite outro número inteiro: ");
                         numInt2 = Integer.parseInt(auxiliar);
                         adi1.somar(numInt1, numInt2);
                         break;
                     case 2:
                         auxiliar = JOptionPane.showInputDialog("Dígite um número real: ");
-                        auxiliar = JOptionPane.showInputDialog("Dígite outro número real: ");
                         numReal1 = Double.parseDouble(auxiliar);
+                        auxiliar = JOptionPane.showInputDialog("Dígite outro número real: ");
                         numReal2 = Double.parseDouble(auxiliar);
                         adi1.somar(numReal1, numReal2);
                         break;
+                    default:
+                        throw new Exception("Escolha Incorreta!!");
                 }
+                escolha = JOptionPane.showInputDialog("Deseja continuar?");
             } catch (Exception e){
                 JOptionPane.showMessageDialog(null, e.getMessage());
             }
         }
+        JOptionPane.showMessageDialog(null, "Fim de Programa, Volte sempre!!");
     }
 }
