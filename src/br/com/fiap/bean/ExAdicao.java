3f615831e06af5b2f7bab0195db6d1ef8d1e2987
Rikk <tarikmoussa90@gmial.com>
2026-04-22 11:50:59 -0300

    feat: atv solo e métodos

---
+++ src/br/com/fiap/bean/ExAdicao.java
@@ -0,0 +1,44 @@
+package br.com.fiap.bean;
+
+public class ExAdicao {
+    //Atributos
+    private int numInt1;
+    private int NumInt2;
+    private double numReal1;
+    private double numReal2;
+
+    //Construtor vazio
+    public ExAdicao(){
+    }
+
+    //Métodos getters e setters
+    public int getNumInt1() {
+        return numInt1;
+    }
+    public void setNumInt1(int numInt1) {
+        this.numInt1 = numInt1;
+    }
+    public int getNumInt2() {
+        return NumInt2;
+    }
+    public void setNumInt2(int numInt2) {
+        NumInt2 = numInt2;
+    }
+    public double getNumReal1() {
+        return numReal1;
+    }
+    public void setNumReal1(double numReal1) {
+        this.numReal1 = numReal1;
+    }
+    public double getNumReal2() {
+        return numReal2;
+    }
+    public void setNumReal2(double numReal2) {
+        this.numReal2 = numReal2;
+    }
+
+    //Métodos da classe
+    public void somar ( int numInt1, int numInt2){
+
+    }
+}
