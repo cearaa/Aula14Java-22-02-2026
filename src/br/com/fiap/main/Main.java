3cb02ef79a51ee7e905e00e4afbf44a2065cffee
Rikk <tarikmoussa90@gmial.com>
2026-04-22 11:50:59 -0300

    feat: CS implemented and finish

--- src/br/com/fiap/main/Main.java
+++ src/br/com/fiap/main/Main.java
@@ -17,18 +17,30 @@ public class Main {
                 opcao = Integer.parseInt(auxiliar);
                 switch (opcao){
                     case 1:
+                        auxiliar = JOptionPane.showInputDialog("Dígite o valor do lado: ");
+                        lado = Float.parseFloat(auxiliar);
+                        geo1.calcularArea(lado);
                         break;
                     case 2:
+                        auxiliar = JOptionPane.showInputDialog("Dígite o valor do lado: ");
+                        lado = Float.parseFloat(auxiliar);
+                        auxiliar = JOptionPane.showInputDialog("Dígite o valor da altura");
+                        altura = Float.parseFloat(auxiliar);
+                        geo1.calcularArea(lado);
                         break;
                     case 3:
+                        auxiliar = JOptionPane.showInputDialog("Dígite o valor do raio: ");
+                        raio = Double.parseDouble(auxiliar);
+                        geo1.calcularArea(raio);
                         break;
                     default:
+                        throw new Exception("Escolha incorreta!!");
                 }
                 escolha = JOptionPane.showInputDialog("Deseja continuar?");
             } catch (Exception e) {
                 JOptionPane.showMessageDialog(null, e.getMessage());
             }
         }
-        JOptionPane.showMessageDialog(null, "Fim de Programa");
+        JOptionPane.showMessageDialog(null, "Fim de Programa, Volte sempre!!");
     }
 }
