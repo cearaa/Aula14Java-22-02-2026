52d8679f6bf7f7a7b86c5a5fa84b4ee44e40e9d7
Rikk <tarikmoussa90@gmial.com>
2026-04-22 16:44:47 -0300

    implementação dos blocos de repetição

--- src/br/com/fiap/main/MainAd.java
+++ src/br/com/fiap/main/MainAd.java
@@ -2,9 +2,37 @@ package br.com.fiap.main;
 
 import br.com.fiap.bean.ExAdicao;
 
+import javax.swing.*;
+
 public class MainAd {
     public static void main(String[] args) {
         ExAdicao adi1 = new ExAdicao();
         String auxiliar, escolha = "sim";
+        int opcao, numInt1, numInt2;
+        double numReal1, numReal2;
+        while (escolha.equalsIgnoreCase("sim")){
+            try {
+                auxiliar = JOptionPane.showInputDialog("Qual tipo de soma deseja calcular? \n1. Números inteiros \n2. Números reais");
+                opcao = Integer.parseInt(auxiliar);
+                switch (opcao){
+                    case 1:
+                        auxiliar = JOptionPane.showInputDialog("Dígite um número inteiro: ");
+                        auxiliar = JOptionPane.showInputDialog("Dígite outro número inteiro: ");
+                        numInt1 = Integer.parseInt(auxiliar);
+                        numInt2 = Integer.parseInt(auxiliar);
+                        adi1.somar(numInt1, numInt2);
+                        break;
+                    case 2:
+                        auxiliar = JOptionPane.showInputDialog("Dígite um número real: ");
+                        auxiliar = JOptionPane.showInputDialog("Dígite outro número real: ");
+                        numReal1 = Double.parseDouble(auxiliar);
+                        numReal2 = Double.parseDouble(auxiliar);
+                        adi1.somar(numReal1, numReal2);
+                        break;
+                }
+            } catch (Exception e){
+                JOptionPane.showMessageDialog(null, e.getMessage());
+            }
+        }
     }
 }
