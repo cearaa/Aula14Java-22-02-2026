2023e79d2927ae80a054e3002609e6cd55af2cea
Rikk <tarikmoussa90@gmial.com>
2026-04-22 11:25:24 -0300

    feat: Estruturas de repetição

--- src/br/com/fiap/main/Main.java
+++ src/br/com/fiap/main/Main.java
@@ -1,7 +1,24 @@
 package br.com.fiap.main;
 
+import br.com.fiap.bean.Geometria;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        Geometria geo1 = new Geometria();
+        String auxiliar, escolha = "sim";
+        int opcao;
+        float lado, altura;
+        double raio;
+        while (escolha.equalsIgnoreCase("sim")){
+            try {
+                auxiliar = JOptionPane.showInputDialog("Qual área deseja calcular? \n1. QUADRADO \n2. RETÂNGULO \n3. CÌRCULO");
+                opcao = Integer.parseInt(auxiliar);
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e.getMessage());
+            }
+        }
+        JOptionPane.showMessageDialog(null, "Fim de Programa");
     }
 }
